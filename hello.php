a30a6d8e36166a323af0d1107563412bf5d0566b
Brendan Fink <bafz86@umsystem.edu>
2019-09-25 13:30:38 -0500

    PHP Page and Style Sheet

---
+++ hello.php
@@ -0,0 +1,18 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+  <link rel='stylesheet' href='style.css' type='text/css' />
+ </head>
+ <body>
+ <div id="fixedheader">Top div content
+ 
+ <div id="fixedbody">
+ <h1>Page Heading<h1>
+ <p>Content placeholder ...</p>
+ </div>
+</div>
+ 
+ <div id="fixedfooter">Bottom div content</div>
+ <?php echo '<p>Hello World</p>'; ?> 
+ </body>
+</html>
