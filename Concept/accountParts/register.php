3610204cef79a8ebdb47c2fcc59532e6e3fdc9bb
Edward Koch <eddiesax598@gmail.com>
2019-10-12 16:25:07 -0500

    Updated Registration Page

--- Concept/accountParts/register.php
+++ Concept/accountParts/register.php
@@ -7,6 +7,8 @@ if (!isset($_SESSION)) session_start();
 require_once "../database/config.php";
 
 #Define variables as Empty
+$fName = "";
+$lName = "";
 $username = "";
 $password = "";
 $confirmPassword = "";
@@ -14,6 +16,8 @@ $email = "";
 $confirmEmail = "";
 $phoneNumber = "";
 
+$fNameError = "";
+$lNameError = "";
 $usernameError = "";
 $passwordError = "";
 $confirmPasswordError = "";
@@ -51,22 +55,47 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     #Close statment
     mysqli_stmt_close($stmt);
   }
+  #Validate First name
+  if(empty(trim($_POST["firstName"]))) {
+    $fNameError = "Please enter your First Name.";
+  } else {
+    $fName = trim($_POST["firstName"]);
+  }
+  #Validate Last name
+  if(empty(trim($_POST["lastName"]))) {
+    $lNameError = "Please enter your Last Name.";
+  } else {
+    $lName = trim($_POST["lastName"]);
+  }
+  #Validate Username
+  if(empty(trim($_POST["username"]))) {
+    $usernameError = "Please enter a username.";
+  } else {
+    $username = trim($_POST["username"]);
+  }
+  #Validate PhoneNumber
+  if(empty(trim($_POST["phoneNumber"]))) {
+    $phoneNumberError = "Please enter a phone number.";
+  } else {
+    $phoneNumber = trim($_POST["phoneNumber"]);
+  }
   #Validate Password
   if(empty(trim($_POST["password"]))) {
     $passwordError = "Please enter a password.";
   } elseif(strlen(trim($_POST["password"])) < 8) {
     $passwordError = "Password must be at least 8 characters.";
+  #} elseif (!preg_match('/^(?=[a-z])(?=[A-Z])[a-zA-Z]{8,}$/', trim($_POST["password"]))) {
+    #$passwordError = "Password must contain at least 1 lowercase letter, 1 uppercase letter and 1 number.";
   } else {
     $password = trim($_POST["password"]);
   }
   #Validate confirm Password
   if(empty(trim($_POST["confirmPassword"]))) {
     $confirmPasswordError = "Please confirm password.";
+  } elseif (trim($_POST["password"]) != trim($_POST["confirmPassword"])) {
+    $confirmPasswordError = "Passwords do not match.";
   } else {
     $confirmPassword = trim($_POST["confirmPassword"]);
-    if(empty($passwordError) && ($password != $confirmPassword)) {
-      $confirmPasswordError = "Passwords do not match.";
-    }
   }
   #Validate Email
   if(empty(trim($_POST["email"]))) {
@@ -139,36 +168,49 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
   <div class="main">
     <!--Section 1-->
     <div class="section1">
-      <div style="width: 350px;">
+      <div>
         <h2>Register User</h2>
         <p>Complete this form to create an account.</p>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-          <div class="form-group <?php echo (!empty($usernameError)) ? 'has-error' : ''; ?>">
+          <div class="form-group <?php echo (!empty($fNameError)) ? 'has-error' : ''; ?>" style="width: 350px; display: inline-block;">
+            <label>First Name</label>
+            <input type="text" name="firstName" class="form-control" value="<?php echo $fName; ?>">
+            <span class="help-block"><?php echo $fNameError; ?></span>
+          </div>
+          <div class="form-group <?php echo (!empty($lNameError)) ? 'has-error' : ''; ?>" style="width: 350px; display: inline-block;">
+            <label>Last Name</label>
+            <input type="text" name="lastName" class="form-control" value="<?php echo $lName; ?>">
+            <span class="help-block"><?php echo $lNameError; ?></span>
+          </div>
+          <br>
+          <div class="form-group <?php echo (!empty($usernameError)) ? 'has-error' : ''; ?>" style="width: 350px; display: inline-block;">
             <label>Username</label>
             <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
             <span class="help-block"><?php echo $usernameError; ?></span>
           </div>
-          <div class="form-group <?php echo (!empty($phoneNumberError)) ? 'has-error' : ''; ?>">
-            <label>Phone Number</label>
-            <input type="text" name="phoneNumber" class="form-control" value="<?php echo $phoneNumber; ?>">
+          <div class="form-group <?php echo (!empty($phoneNumberError)) ? 'has-error' : ''; ?>" style="width: 350px; display: inline-block;">
+            <label>Phone Number (123-456-7890)</label>
+            <input type="tel" placeholder="XXX-XXX-XXXX" maxlength="12" name="phoneNumber" class="form-control" value="<?php echo $phoneNumber; ?>">
             <span class="help-block"><?php echo $phoneNumberError; ?></span>
           </div>
-          <div class="form-group <?php echo (!empty($emailError)) ? 'has-error' : ''; ?>">
+          <br>
+          <div class="form-group <?php echo (!empty($emailError)) ? 'has-error' : ''; ?>" style="width: 350px; display: inline-block;">
             <label>Email</label>
-            <input type="text" name="email" class="form-control" value="<?php echo $email; ?>">
+            <input type="email" name="email" class="form-control" value="<?php echo $email; ?>">
             <span class="help-block"><?php echo $emailError; ?></span>
           </div>
-          <div class="form-group <?php echo (!empty($confirmEmailError)) ? 'has-error' : ''; ?>">
+          <div class="form-group <?php echo (!empty($confirmEmailError)) ? 'has-error' : ''; ?>" style="width: 350px; display: inline-block;">
             <label>Confirm Email</label>
-            <input type="text" name="confirmEmail" class="form-control" value="<?php echo $confirmEmail; ?>">
+            <input type="email" name="confirmEmail" class="form-control" value="<?php echo $confirmEmail; ?>">
             <span class="help-block"><?php echo $confirmEmailError; ?></span>
           </div>
-          <div class="form-group <?php echo (!empty($passwordError)) ? 'has-error' : ''; ?>">
+          <br>
+          <div class="form-group <?php echo (!empty($passwordError)) ? 'has-error' : ''; ?>" style="width: 350px; display: inline-block;">
             <label>Password</label>
             <input type="password" name="password" class="form-control" value="<?php echo $password; ?>">
             <span class="help-block"><?php echo $passwordError; ?></span>
           </div>
-          <div class="form-group <?php echo (!empty($confirmPasswordError)) ? 'has-error' : ''; ?>">
+          <div class="form-group <?php echo (!empty($confirmPasswordError)) ? 'has-error' : ''; ?>" style="width: 350px; display: inline-block;">
             <label>Confirm Password</label>
             <input type="password" name="confirmPassword" class="form-control" value="<?php echo $confirmPassword; ?>">
             <span class="help-block"><?php echo $confirmPasswordError; ?></span>
