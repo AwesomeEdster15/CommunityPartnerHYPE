1717b4f9e9c92ca0763c9899d7e60baa60ea8140
Zach Bax <zpbtg6@mst.edu>
2019-10-13 15:23:01 -0500

    updated about page

--- Concept/mainPages/aboutUs.php
+++ Concept/mainPages/aboutUs.php
@@ -6,9 +6,16 @@
     <!--Section 1-->
     <div class="section1">
       <div>
-        <h1 class="title">Hello!</h1>
+        <h1 class="title">About Capable Kids And Families®</h1>
         <p>
-          This is where you can Learn about the organization!
+          Families of children with developmental delays and disabilities often report experiencing isolation, exclusion, financial hardships, insurance restrictions and difficulty accessing treatment, services, and resources.
+          Capable Kids and Families® works to increase children’s developmental gains while supporting the family with the appropriate resources and connections.
+        </p>
+        <p>
+          Professional therapy equipment is loaned to families at no charge to provide parents with the tools to help their child progress!  Home visitors work closely with an enrolled child’s therapists to ensure the correct equipment is placed in the child’s home.  Equipment is delivered to families by CKF staff.
+        </p>
+        <p>
+          CKF also provides a playroom for enrolled families.  The playroom includes a Lite-Gait Walkable, a Somatron Multi-Sensory Ball Pool, an Equipony (simulates therapeutic horseback riding) and therapy swings.  Enrolled families can reserve the room to use with their child’s therapist or for their family.    
         </p>
       </div>
     </div>
