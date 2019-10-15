7a325ba3c097219694cd208a18bf8c8376701655
bmccurry <bmmnt4@mst.edu>
2019-10-15 17:21:23 -0500

    Update adminPage.php
    Fixed the admin page

--- Concept/adminParts/adminPage.php
+++ Concept/adminParts/adminPage.php
@@ -1,8 +1,6 @@
 <!--Header-->
 
 <?php
-#make firstName, LastName, email, and isAdmin editable
-#Register page
 #<!--Initialize Session-->
 if (!isset($_SESSION)) session_start();
 
@@ -13,9 +11,11 @@ require_once "../database/config.php";
 ?>
 
 <?php require "../sharedParts/header.php"; ?>
-
   <!---MAIN----->
   <div class="main">
+
+
+
     <!--Section 1-->
     <div class="section1">
       <div>
@@ -25,6 +25,9 @@ require_once "../database/config.php";
         </p>
       </div>
     </div>
+
+
+
     <!--Section 2-->
     <section>
     	<table class="adminTable" cellspacing="10">
@@ -35,8 +38,9 @@ require_once "../database/config.php";
     			<td>phoneNumber</td>
     			<td>isAdmin</td>
     		</tr>
+
+    		<!-- List all of the admins -->
     		<?php
-    			#should I make the entire patron table editable?
     			$username = "";
     			$usernameError = "";
 				$query = mysqli_query($dbCon, "select * from Patron");
@@ -44,27 +48,32 @@ require_once "../database/config.php";
     			{
     				if ($result["isAdmin"])
     				{
+	    				# create a table to store the information 
 	    				echo "<tr>";
 	    				echo "<td>".$result["userName"]."</td>";
 	    				echo "<td><input type='text' value='".$result["email"]."'></td>";
 	    				echo "<td><input type='text' value='".$result["phoneNumber"]."'></td>";
+	    				
+	    				# check the box if the user is an admin.
+	    				# All should be admin, but did this in case of future changes.
 	    				if($result["isAdmin"])
-						{
 							echo "<td><input type='checkbox' checked></td>";
-						}
 						else
-						{
 							echo "<td><input type='checkbox' unchecked></td>";
-						}
-	    				echo "<td><input type='submit' class='btn btn-primary' value='Submit'></td>";
+
+						# The Submit button at the end of each line
+						echo "<td><input type='submit' class='btn btn-primary' value='Submit'></td>";
 	    				echo "</tr>";
 	    			}
     			}
     		?>
 		</table>
+
+		<!-- created a way to lookup and edit info for any user -->
 		<label>USER LOOKUP</label>
 		<table class="userTable" cellspacing="10">
 			<tr>
+			<!-- the form for the user lookup, with isset name userChoice -->
     		<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
     			<div class="form-group <?php echo (!empty($usernameError)) ? 'has-error' : ''; ?>" style="width: 350px; display:inline;">
 	    			<td><input type='text' name='username' class='form-control' value="<?php echo $username; ?>"></td>
@@ -73,11 +82,12 @@ require_once "../database/config.php";
 		    	</div>
 		    </form>
     		</tr>
+
+
     		<?php
     			if($_SERVER["REQUEST_METHOD"] == "POST") {
     				if (isset($_POST['userChoice']))
 			    	{
-			    		echo "<label>ooo</label>";
 	    				if(empty(trim($_POST["username"]))) {
 	    					$usernameError = "Please enter a username.";
 	    				}
@@ -99,7 +109,7 @@ require_once "../database/config.php";
 								echo "<form action='".htmlspecialchars($_SERVER["PHP_SELF"])."' method='post' name='updateUser'>";
 				    			echo "<div class='form-group' style='width: 350px; display:inline;'>";
 								echo "<tr>";
-								echo "<td>".$result['userName']."</td>";
+								echo "<td><input type='text' name='userName' value='".$result['userName']."'</input></td>";
 								echo "<td><input type='text' name='email' value='".$result["email"]."'></td>";
 								echo "<td><input type='text' name='phoneNumber' value='".$result["phoneNumber"]."'></td>";
 								if($result["isAdmin"])
@@ -115,83 +125,53 @@ require_once "../database/config.php";
 								echo "</tr>";
 				    			echo "</div>";
 				    			echo "</form>";
-
-				    			if (isset($_POST['update']))
-				    			{
-				    				echo "<label>hello</label>";
-				    				if(empty(trim($_POST["email"])))
-				    				{
-	    								$usernameError = "Please enter a email.";
-	    								echo "<label>$usernameError hi</label>";
-	    							}
-	    							else if(empty(trim($_POST["phoneNumber"])))
-	    							{
-	    								$usernameError = "Please enter a phone number.";
-	    								echo "<label>$usernameError hello</label>";
-	    							}
-	    							else
-	    							{
-	    								$paramUsername = $result['userName'];
-	    								$paramEmail = trim($_POST["email"]);
-	    								$paramPhoneNumber = trim($_POST["phoneNumber"]);
-	    								$paramIsAdmin = $_POST["isAdmin"];
-	    								echo "<label>$paramUsername oh</label>";
-	    								$insertUserSQL = "UPDATE Patron SET email = ?, phoneNumber = ?, isAdmin = ? WHERE userName = ?";
-	    								if($stmt = mysqli_prepare($dbCon, $insertUserSQL))
-	    								{
-	    									mysqli_stmt_bind_param($stmt, "ssis", $paramEmail, $paramPhoneNumber, $paramIsAdmin, $paramUsername);
-	    									if(mysqli_stmt_execute($stmt))
-	    									{
-	    										echo "Success!";
-	    										#header("location: adminPage.php");
-	    									}
-	    									else
-	    									{
-	    										echo "Oops! Something went wrong. Please try again later.";
-	    									}
-	    								}
-	    							}
-	    						}
 				    		}
 				    		else
 				    		{
 				    			$usernameError = "User does not exist";
 				    		}
-							/*
+						}
+					}
 
-			    			$userSQL = "SELECT * FROM patron WHERE userName = 'timbone'";
-			    			$SQLuserName = "";
-			    			$SQLemail = "";
-			    			$SQLpassWord = "";
-			    			$SQLphoneNumber = "";
-			    			$SQLisAdmin = "";
-			    			$SQLuh = "";
-			    			if($nonAdmin = mysqli_prepare($dbCon, $userSQL))
-			    			{
-			    				#mysqli_stmt_bind_param($nonAdmin, "s", $paramUsername);
-			    				$paramUsername = "'".trim($_POST["username"])."'";
-									echo "<label>$paramUsername</label>";
-			    				if(mysqli_stmt_execute($nonAdmin))
-			    				{
-									mysqli_stmt_store_result($nonAdmin);
-									mysqli_stmt_bind_result($nonAdmin, $SQLuserName, $SQLpassWord, $SQLemail, $SQLphoneNumber, $SQLisAdmin);
-									echo "<tr>";
-									echo "<td><input type='text' value='".$SQLuserName."'></td>";
-									echo "<td><input type='text' value='".$SQLemail."'></td>";
-									echo "<td><input type='text' value='".$SQLphoneNumber."'></td>";
-									if($SQLisAdmin)
-									{
-										echo "<td><input type='checkbox' checked></td>";
-									}
-									else
-									{
-										echo "<td><input type='checkbox' unchecked></td>";
-									}
-									echo "<td><input type='submit' class='btn btn-primary' value='Submit'>";
-									echo "</tr>";
+					# Update the user info
+					if (isset($_POST['update']))
+					{
+						#error checking for Null, need to add data constraints
+						if(empty(trim($_POST["email"])))
+						{
+							echo "Please enter a email.";
+						}
+						else if(empty(trim($_POST["phoneNumber"])))
+						{
+							echo "Please enter a phone number.";
+						}
+						else
+						{
+							$paramUsername = trim($_POST["userName"]);
+							$paramEmail = trim($_POST["email"]);
+							$paramPhoneNumber = trim($_POST["phoneNumber"]);
+							if ($_POST["isAdmin"] == "on")
+							{
+								$paramIsAdmin = 1;
+							}
+							else
+							{
+								$paramIsAdmin = 0;
+							}
+							$insertUserSQL = "UPDATE Patron SET email = ?, phoneNumber = ?, isAdmin = ? WHERE userName = ?";
+							if($stmt = mysqli_prepare($dbCon, $insertUserSQL))
+							{
+								mysqli_stmt_bind_param($stmt, "ssis", $paramEmail, $paramPhoneNumber, $paramIsAdmin, $paramUsername);
+								if(mysqli_stmt_execute($stmt))
+								{
+									echo "Success!";
+									#header("location: adminPage.php");
+								}
+								else
+								{
+									echo "Oops! Something went wrong. Please try again later.";
 								}
 							}
-							*/
 						}
 					}
 				}
