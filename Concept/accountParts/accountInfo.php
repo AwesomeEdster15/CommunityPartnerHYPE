b4029c1a5ab6569e8fe34659875f32bcb95dc115
Edward Koch <eddiesax598@gmail.com>
2019-10-15 15:21:49 -0500

    registers fname and linitial, user can view own info

--- Concept/accountParts/accountInfo.php
+++ Concept/accountParts/accountInfo.php
@@ -9,6 +9,23 @@
       <p>
         This is where you can view or edit your account information!
       </p>
+      <div style="width: 350px; display: inline-block;">
+        <h3>Name</h3>
+        <h4> <?php echo $_SESSION["firstName"] . " " . $_SESSION["lastInitial"] ?> </h4>
+      </div>
+      <div style="width: 350px; display: inline-block;">
+        <h3>Username</h3>
+        <h4> <?php echo $_SESSION["username"] ?></h4>
+      </div>
+      <br>
+      <div style="width: 350px; display: inline-block;">
+        <h3>Email</h3>
+        <h4> <?php echo $_SESSION["email"] ?> </h4>
+      </div>
+      <div style="width: 350px; display: inline-block;">
+        <h3>Phone Number</h3>
+        <h4> <?php echo $_SESSION["phoneNumber"] ?></h4>
+      </div>
     </div>
   </div>
   <!--Section 2-->
