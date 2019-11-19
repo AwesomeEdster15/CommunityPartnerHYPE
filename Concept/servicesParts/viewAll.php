0e5085d418a0c93412a3447a9ed37dc0704f12b3
paulfish <fish.paul.111@gmail.com>
2019-11-19 15:03:24 -0600

    Added sort functionality to column headers in viewAll.php.

--- Concept/servicesParts/viewAll.php
+++ Concept/servicesParts/viewAll.php
@@ -25,11 +25,10 @@
             echo "<table id=\"itemTable\">
             <tr>
             <th>Image Link</th>
-            <th>Product Name</th>
-            <th>Product Link</th>
-            <th>Stock Count</th>
-            <th>Reusable</th>
-            <th>Request Period</th>
+            <th onclick=\"sortTable(1) \">Product Name</th>
+            <th onclick=\"sortTable(2) \">Stock Count</th>
+            <th onclick=\"sortTable(3) \">Reusable</th>
+            <th onclick=\"sortTable(4) \">Request Period</th>
             </tr>";
 
             while($row = mysqli_fetch_array($result))
@@ -39,7 +38,6 @@
             $product = $row['productLink'];
             echo "<td> <a href=\"$product\"><img src=\"$image\" width=\"100\" height=\"100\" /></a> </td>";
             echo "<td><a href=\"viewProductType.php?productName=" . $row['productName'] . "\">" . $row['productName'] . "</a></td>";
-            echo "<td>" . $row['productLink'] . "</td>";
             echo "<td>" . $row['stockCount'] . "</td>";
             //echo "<td>" . $row['imageLink'] . "</td>";
             echo "<td>" . $row['reusable'] . "</td>";
@@ -70,6 +68,61 @@
               }
             }
           }
+
+          function sortTable(n) {
+              var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+              table = document.getElementById("itemTable");
+              switching = true;
+              // Set the sorting direction to ascending:
+              dir = "asc";
+              /* Make a loop that will continue until
+              no switching has been done: */
+              while (switching) {
+                // Start by saying: no switching is done:
+                switching = false;
+                rows = table.rows;
+                /* Loop through all table rows (except the
+                first, which contains table headers): */
+                for (i = 1; i < (rows.length - 1); i++) {
+                  // Start by saying there should be no switching:
+                  shouldSwitch = false;
+                  /* Get the two elements you want to compare,
+                  one from current row and one from the next: */
+                  x = rows[i].getElementsByTagName("td")[n];
+                  y = rows[i + 1].getElementsByTagName("td")[n];
+                  /* Check if the two rows should switch place,
+                  based on the direction, asc or desc: */
+                  if (dir == "asc") {
+                    if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                      // If so, mark as a switch and break the loop:
+                      shouldSwitch = true;
+                      break;
+                    }
+                  } else if (dir == "desc") {
+                    if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                      // If so, mark as a switch and break the loop:
+                      shouldSwitch = true;
+                      break;
+                    }
+                  }
+                }
+                if (shouldSwitch) {
+                  /* If a switch has been marked, make the switch
+                  and mark that a switch has been done: */
+                  rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                  switching = true;
+                  // Each time a switch is done, increase this count by 1:
+                  switchcount ++;
+                } else {
+                  /* If no switching has been done AND the direction is "asc",
+                  set the direction to "desc" and run the while loop again. */
+                  if (switchcount == 0 && dir == "asc") {
+                    dir = "desc";
+                    switching = true;
+                  }
+                }
+              }
+            }
          </script>
     </section>
     <section style="text-align: center; padding: 15px 0px 15px 0px;">
